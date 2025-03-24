4175c7c558603c7ae5d279e008d46fb4029a4e06
unknown <seshdev01012001@gmail.com>
2025-03-24 14:43:22 +0530

    Second commit

--- chat-app-backend/src/main/java/com/seshadev/chatApp/controllers/RoomController.java
+++ chat-app-backend/src/main/java/com/seshadev/chatApp/controllers/RoomController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.seshadev.chatApp.entities.Message;
@@ -18,54 +19,61 @@ import com.seshadev.chatApp.repo.RoomRepo;
 @RestController
 @RequestMapping("api/v1/room")
 
-public class RoomController {	
+public class RoomController {
 	private RoomRepo roomRepository;
-	
+
 	public RoomController(RoomRepo roomRepository) {
 		super();
 		this.roomRepository = roomRepository;
 	}
 
-
-
 	@PostMapping
-	public ResponseEntity<?> createRoom(@RequestBody String roomId){
-		
-		if(roomRepository.findByRoomId(roomId) != null) {
+	public ResponseEntity<?> createRoom(@RequestBody String roomId) {
+
+		if (roomRepository.findByRoomId(roomId) != null) {
 			return ResponseEntity.badRequest().body("Room already exists");
 		}
-		
+
 		Room room = new Room();
-        room.setRoomId(roomId);
-        Room savedRoom = roomRepository.save(room);
-        return ResponseEntity.status(HttpStatus.CREATED).body(room);
-		
+		room.setRoomId(roomId);
+		Room savedRoom = roomRepository.save(room);
+		return ResponseEntity.status(HttpStatus.CREATED).body(room);
+
 	}
-	
-	
-	@GetMapping("/{roomId}")	
-	public ResponseEntity<?> joinRoom(@PathVariable String roomId){
+
+	@GetMapping("/{roomId}")
+	public ResponseEntity<?> joinRoom(@PathVariable String roomId) {
+
+		Room room = roomRepository.findByRoomId(roomId);
+
+		if (room == null) {
+			return ResponseEntity.badRequest().body("Room not found!!");
+		}
+
+		return ResponseEntity.ok(room);
+	}
+
+	@GetMapping("/{roomId}/messages")
+	public ResponseEntity<List<Message>> getMessages( @PathVariable String roomId, 
+			@RequestParam(defaultValue = "0", required = false) int page,
+			@RequestParam(defaultValue = "20", required = false) int size
+			){
 		
 		Room room = roomRepository.findByRoomId(roomId);
 		
 		if(room == null) {
-			return ResponseEntity.badRequest().body("Room not found!!");
+			return ResponseEntity.badRequest().build();
 		}
 		
-		return ResponseEntity.ok(room);
-	}
-	
-
-	@GetMapping("/{roomId}/messages")
-	public ResponseEntity<List<Message>> getMessages( @PathVariable String roomId){
+		List<Message> messages = room.getMessages();
+		
+		int start = Math.max(0, messages.size() - (page+1)*size);
+		int end = Math.min(messages.size(), start+size);
+		List<Message> paginatedMessages = messages.subList(start, end);
 		
 		
-		
-		return null;
+		return ResponseEntity.ok(paginatedMessages);
 			
 	}
-	
-	
-	
 
 }
